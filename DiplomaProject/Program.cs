544413b6da299cb84403e4961eeab4af0ef3c2c1
Kostia Yeriomin <konstantin.yeriomin.ext@channelpilot.com>
2022-05-15 21:38:09 +0300

    [add_ci] Add ci

--- DiplomaProject/Program.cs
+++ DiplomaProject/Program.cs
@@ -18,6 +18,9 @@ namespace DiplomaProject
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder => {
+                   webBuilder.UseStartup<Startup>();
+                   webBuilder.UseUrls("http://*:5005");
+                });
     }
 }
