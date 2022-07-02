bf8dbe6c346362eea3e375e78844d89fa5e16196
Kostia Yeriomin <konstantin.yeriomin.ext@channelpilot.com>
2022-07-02 21:06:34 +0300

    Add cert

--- DiplomaProject/Program.cs
+++ DiplomaProject/Program.cs
@@ -16,6 +16,7 @@ namespace DiplomaProject
             CreateHostBuilder(args).Build().Run();
         }
 
+        
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
