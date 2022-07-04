81f950c5f10e23be982c0d808f14763300b39bd7
Kostia Yeriomin <konstantin.yeriomin.ext@channelpilot.com>
2022-07-04 14:05:09 +0300

    Add logs

--- DiplomaProject/Startup.cs
+++ DiplomaProject/Startup.cs
@@ -87,13 +87,14 @@ namespace DiplomaProject
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                // app.UseDeveloperExceptionPage();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
